3c18dfcf56bb2f2b51a1e72a65d6d4f4b6e0b433
chrisd95 <34041303+chrisd95@users.noreply.github.com>
2019-04-06 12:02:02 -0400

    Update NP.py

--- NP.py
+++ NP.py
@@ -1,5 +1,7 @@
 sentence = "A lion attacked Jane in the zoo"
 
+print("Raw Data: " + sentence)
+
 dictionnary = {
 "lion" : "N",
 "knife": "N",
@@ -11,6 +13,17 @@ dictionnary = {
 "with":"PREP",
 "in":"PREP"
 };
+
+grammar = {
+"NPVP": "S",
+"V": "VP",
+"VNP":"VP",
+"VPPP":"VP",
+"DETN":"NP",
+"PN":"NP",
+"NPPP":"NP",
+"PREPNP":"PP"
+}
 
 def sentenceLister(sentence):
     sentenceList = []
@@ -30,7 +43,7 @@ def sentenceLister(sentence):
 
 sentenceList = sentenceLister(sentence)
 
-print(sentenceList)
+print("Listify: " + str(sentenceList))
 
 def parser(sentenceList):
     parsedSentence = []
@@ -40,4 +53,38 @@ def parser(sentenceList):
     return parsedSentence
 parsedSentence = parser(sentenceList)
 
-print(parsedSentence)
+print("Parsed: " + str(parsedSentence))
+
+def parseTrees(parsedSentence):
+    matchedparsedSentence = []
+    matchedparsedSentence2 = []
+    for i in range(len(parsedSentence)):
+        if i%2==0 and i < len(parsedSentence)-1:
+            match = parsedSentence[i] + parsedSentence[i+1]
+            matchedparsedSentence.append(match)
+    if len(parsedSentence)%2 == 1:
+        matchedparsedSentence.append(parsedSentence[-1])
+
+    if len(parsedSentence)%2 == 1:
+        matchedparsedSentence2.append(parsedSentence[0])
+    for i in range(len(parsedSentence)):
+        if i%2==1 and i < len(parsedSentence)-1:
+            match = parsedSentence[i] + parsedSentence[i+1]
+            matchedparsedSentence2.append(match)
+    if len(parsedSentence)%2 == 1:
+        matchedparsedSentence2.append(parsedSentence[-1])
+
+    print("Bottom-up 1: " + str(matchedparsedSentence))
+
+    print("Bottom-up 2: " + str(matchedparsedSentence2))
+
+    for i in range(len(matchedparsedSentence)):
+        if matchedparsedSentence[i] in grammar:
+            lookUp = grammar[matchedparsedSentence[i]]
+            matchedparsedSentence[i] = lookUp
+
+    print(matchedparsedSentence)
+
+
+
+parseTrees(parsedSentence)
